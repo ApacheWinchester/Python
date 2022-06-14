39ba5eb86158d395223d2e77c2ccc8d29bba2cde
Administrador <henriquesatoszan@hotmail.com>
2022-06-14 07:53:56 -0300

    Tmaking changes to dev

---
+++ exercicios/ex001.py
@@ -0,0 +1,3 @@
+# Hello world
+nome = str(input("Bom dia meu querido(a) poderia nos informar seu nome por favor?  "))
+print("Muito bem, otimo que você esteja aqui {}, tenha um bom dia, uma boa tarde e uma otima noite!!!!".format(nome))
---
+++ exercicios/ex002.py
@@ -0,0 +1,4 @@
+# Boas Vindas
+
+nome = str(input("Olá, poderia me informar seu nome por favor ?  "))
+print("Seja bem vindo, ficamos felizes com sua vinda {}, espero que esteja bem e que sejá um ótimo dia para todo".format(nome))
