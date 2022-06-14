30a02e696a48716cff51426d758011a24ba61e15
Administrador <henriquesatoszan@hotmail.com>
2022-06-14 14:05:21 -0300

    Tmaking changes to dev

---
+++ exercicios/ex003.py
@@ -0,0 +1,6 @@
+# soma de dois números
+n1 = float(input("Digite o valor do primeiro número: "))
+n2 = float(input("Digite o valor do segundo número: "))
+print("A soma dos números {:.1f} e {:.1f} é {:.1f}".format(n1, n2, n1+n2))
+
+
