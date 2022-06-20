eef7d33fba8515a5f186fbf0662c9eca5e8abf65
Administrador <henriquesatoszan@hotmail.com>
2022-06-20 14:48:03 -0300

    teste de um app

---
+++ exercicios/ex005.py
@@ -0,0 +1,3 @@
+# Antecessor e sucessor
+n = int(input("Digite algum número para que seja mostrado  o antecessor e o seu sucessor  "))
+print("O seu antecessor do número {}  é {} e o sucessor é {}".format(n, n-1, n+1))
---
+++ exercicios/ex006.py
@@ -0,0 +1,4 @@
+# dobro , triplo, raiz quadrada
+from math import sqrt
+n = int(input("Digite o número que irá ter mostrado seu dobro, seu triplo e sua rapz quadrada  "))
+print("A o dobro do número {}, é {}, seu triplo é {} e sua raiz quadrada é {}".format(n, n ** 2, n ** 3, sqrt(n)))
