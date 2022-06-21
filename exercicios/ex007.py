1883529d35a6d67eb6fd385da557aaa6f289e56c
Administrador <henriquesatoszan@hotmail.com>
2022-06-21 15:55:10 -0300

    teste de um app

---
+++ exercicios/ex007.py
@@ -0,0 +1,5 @@
+# média aritmética
+n1 = float(input("Digite o valor da primeira nota "))
+n2 = float(input("Digite o valor da segunda nota "))
+m = n1 + n2 / 2
+print("A média entre as notas {} e {} é {}".format(n1, n2, m))
---
+++ exercicios/ex008.py
@@ -0,0 +1,2 @@
+# conversor de medidas
+b = int(input("Digite o valor  que deseja ver a conversão"))
