bc294998523950cff7253372d5d56647f0c41970
Administrador <henriquesatoszan@hotmail.com>
2022-06-22 12:31:10 -0300

    Conversor de medidas

--- exercicios/ex008.py
+++ exercicios/ex008.py
@@ -1,2 +1,4 @@
 # conversor de medidas
-b = int(input("Digite o valor  que deseja ver a conversão"))
+b = int(input("Digite o valor  que deseja ver a conversão "))
+print("{} metros é equivalente á {} decâmetros(dam) \n á {} Hectômetro(hm) \n á {} Quilômetros(km) \n á {} De´cimetros(dm) \n á {} Centímetros(cm) \n á {} Milímetros(mm)"
+      .format(b, b * 10, b *100, b * 1000, b / 10, b / 100, b / 1000))
