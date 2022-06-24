f589720735e301f5a701aeb5348e086b80e1cc2f
Administrador <henriquesatoszan@hotmail.com>
2022-06-24 15:32:52 -0300

    Compra de Dollar

--- exercicios/ex010.py
+++ exercicios/ex010.py
@@ -1,44 +1,14 @@
-import requests
+# Compra de Dollar
+import time
+from datetime import datetime
 from bs4 import BeautifulSoup
+import requests
+dolar = requests.get("https://www.google.com/search?client=opera-gx&q=valor+do+dolar&sourceid=opera&ie=UTF-8&oe=UTF-8").content
+soup = BeautifulSoup(dolar, 'dolar.parser')
+print(soup.prettify())
 
-# pagina que vamos trabalhar
-url = 'https://www.pichau.com.br/hardware/placa-de-video'
 
-headers = {
-    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36 OPR/87.0.4390.58"}
-
-site = requests.get(url, headers=headers)
-soup = BeautifulSoup(site.content, 'html.parser')
-placas = soup.find_all('div', class_='product details product-item-details')
-ultima_pagina = soup.find('span', class_='text-page last').get_text().strip()
-
-for i in range(1, int(ultima_pagina)):
-    url_pag = f'https://www.pichau.com.br/hardware/placa-de-video?p={i}'
-    site = requests.get(url_pag, headers=headers)
-    soup = BeautifulSoup(site.content, 'html.parser')
-    placas = soup.find_all('div', class_='product details product-item-details')
-
-    with open('precos_placas.csv', 'a', newline='', encoding='utf-8') as f:
-        for placa in placas:
-
-            marca = placa.find('a', class_='product-item-link').get_text().strip()
-            try:
-                preco = placa.find('span', class_='price').get_text().strip()
-                num_preco = preco[2:]
-                num_preco = num_preco[:-3]
-
-            except:
-                num_preco = '0'
-
-            try:
-                preco_boleto = placa.find('span', class_='price-boleto').get_text().strip()
-                index = preco_boleto.find(',')
-                num_preco_boleto = preco_boleto[10:index]
-
-            except:
-                num_preco_boleto = '0'
-
-            linha = marca + ';' + num_preco + ';' + num_preco_boleto + '\n'
-            print(linha)
-            f.write(linha)
-    print(url_pag)
+#data = datetime.today().strftime('%A-%B-%d-%Y')
+#real = float(input("Deseja trocar quantos reais em dollar? "))
+#time.sleep(1.5)
+#print("Nesta data {},  o dolar se encontra á {} reais".format(data, data))
